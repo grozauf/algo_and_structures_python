b2f0d1fc71eb810c3a2cd206db2189298afa86a5
Pavel Lobanov <lobanov.ps@gmail.com>
2019-02-28 21:16:32 +0300

    Домашнее задание для четвёртого урока

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,62 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+"""
+Решается следующая задача из второго урока:
+4.  Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
+Количество элементов (n) вводится с клавиатуры.
+
+Рассматриваются два варианта: через цикл и через рекурсию
+Cложность алгоритма: O(n)
+"""
+
+import timeit
+import cProfile
+
+def cycle_sum(n):
+    res = 1
+    elem = 1
+
+    for i in range(1, n):
+        elem *= -0.5
+        res += elem
+
+    return res
+
+def recursion_sum(n, elem):
+    if n == 1:
+        return elem
+    else:
+        return elem+recursion_sum(n-1, elem * -0.5)
+
+n = 600
+
+print("Сумма ряда (вычисление через цикл): %f" % cycle_sum(n))
+print("Сумма ряда (рекурсивное вычисление): %f" % recursion_sum(n, 1))
+
+print("время вычисления через цикл:", timeit.timeit(("cycle_sum(n)"), setup="from __main__ import cycle_sum, n"))
+print("время вычисления через рекурсию:", timeit.timeit(("recursion_sum(n, 1)"), setup="from __main__ import recursion_sum, n"))
+"""
+timeit для цикла при n = 600 и 1 млн. повторений показывает время - 49.20 с
+timeit для рекурсии при n = 600 и 1 млн. повторений показывает время - 170.49 с
+"""
+
+n = 900
+
+cProfile.run('cycle_sum(n)')
+cProfile.run('recursion_sum(n, 1)')
+"""
+вывод cProfile для цикла при n = 900:
+       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+            1    0.000    0.000    0.000    0.000 1.py:18(cycle_sum)
+        ...
+        т.е. нулевое время и один вызов
+
+вывод cProfile для рекурсии при n = 900:
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+    900/1    0.001    0.000    0.001    0.001 1.py:28(recursion_sum)
+    ...
+    т.е. время выполнения 0.001 с и 900 вызовов функции
+
+"""
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,79 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+
+"""
+сложность алгоритма "Решето Эратосфена" при составлении таблицы простых чисел до n: O(n*log(log(n)))
+"""
+
+import timeit
+import cProfile
+
+def find_prime_erotosfen(i):
+    if i > 1:
+        n = i ** 2
+    else:
+        n = 3
+    a = [x for x in range(n)]  
+
+    a[1] = 0
+
+    m = 2
+    cur_count = 0
+    while m < n:
+        if a[m] != 0:
+            cur_count += 1
+            if cur_count == i:
+                return a[m]
+            j = m * 2
+            while j < n:
+                a[j] = 0
+                j = j + m
+        m += 1
+
+def find_prime(i):
+    cur_count = 0
+    n = 2
+    while True:
+        d = 2
+        while d*d <= n and n%d != 0:
+            d += 1
+        if (d*d > n):
+            cur_count += 1
+            if cur_count == i:
+                return n
+        n += 1
+
+
+
+for i in range(1,100):
+    print(i,"find_prime_erotosfen() -> ",find_prime_erotosfen(i), "find_prime() ->", find_prime(i))
+
+i = 10
+print("Время поиска через решето Эратосфена:", timeit.timeit(("find_prime_erotosfen(i)"), setup="from __main__ import find_prime_erotosfen, i"))
+print("Время поиска без решета Эратосфена:", timeit.timeit(("find_prime(i)"), setup="from __main__ import find_prime, i"))
+"""
+timeit для решета Эратосфена при i = 10 и 1 млн. повторений показал время - 21.72 с
+timeit для алгоритма без решета Эратосфена при i = 10 и 1 млн. повторений показал время - 10.58 с
+"""
+
+
+i = 1000
+
+cProfile.run("find_prime_erotosfen(i)")
+cProfile.run("find_prime(i)")
+"""
+вывод cProfile для решета Эратосфена при i = 1000:
+       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+            1    0.313    0.313    0.371    0.371 2.py:10(find_prime_erotosfen)
+            1    0.059    0.059    0.059    0.059 2.py:15(<listcomp>)
+            ...
+
+        т.е. время выполнения функции find_prime_erotosfen() - 0.371 с, причём 0.059 с тратится на создание массива, из которого будут вычёркиваться элементы
+
+вывод cProfile для алгоритма без решета Эратосфена при i = 1000:
+       ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+            1    0.013    0.013    0.013    0.013 2.py:32(find_prime)
+            ...
+        т.е. время выполнения функции find_prime() - 0.013 с.
+"""
