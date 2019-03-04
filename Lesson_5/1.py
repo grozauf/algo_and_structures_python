d8073cfee164aa04ca96a96ee99397274b996108
Pavel Lobanov <lobanov.ps@gmail.com>
2019-03-04 20:59:07 +0300

    Домашнее задание для пятого урока

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -5,3 +5,40 @@
 вывести наименования предприятий, чья прибыль выше среднего и отдельно
 вывести наименования предприятий, чья прибыль ниже среднего.
 """
+
+import collections
+
+n = int(input("Введите количество предприятий: "))
+
+company_year_profit = collections.defaultdict(list)
+for i in range(n):
+    company_name = input("Введите имя %d-го предприятия: " % (i+1))
+    quarter_1 = float(input("Введите прибыль за 1-ый квартал: "))
+    quarter_2 = float(input("Введите прибыль за 2-ой квартал: "))
+    quarter_3 = float(input("Введите прибыль за 3-ий квартал: "))
+    quarter_4 = float(input("Введите прибыль за 4-ый квартал: "))
+
+    company_year_profit[company_name] = quarter_1 + quarter_2 + quarter_3 + quarter_4
+
+average_profit = sum(company_year_profit.values()) / n
+
+print("Среднея годовая прибыль всех компаний: %.2f" % average_profit)
+
+more_profit = []
+less_profit = []
+
+for company in company_year_profit:
+    if company_year_profit[company] > average_profit:
+        more_profit.append(company)
+    elif company_year_profit[company] < average_profit:
+        less_profit.append(company)
+
+if len(more_profit) == 0:
+    print("Нет компаний с прибылью больше средней годовой прибыли всех компаний")
+else:
+    print("Компании с прибылью больше средней годовой прибыли всех компаний:", more_profit)
+
+if len(less_profit) == 0:
+    print("Нет компаний с прибылью меньше средней годовой прибыли всех компаний")
+else:
+    print("Компании с прибылью меньше средней годовой прибыли всех компаний:", less_profit)
--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,33 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+
+def hex_to_dec(hex_list):
+    hex_hash = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7,
+                '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15}
+    result = 0
+    degree = len(hex_list) - 1
+    for x in hex_list:
+        result += hex_hash[x] * 16 ** degree
+        degree -= 1
+    return result
+
+
+def dec_to_hex(number):
+    dec_hash = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7',
+                8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+    result = []
+    if number == 0:
+        return ['0']
+    while number != 0:
+        result.append(dec_hash[number % 16])
+        number = number // 16
+    return result[::-1]
+
+
+a = list(input("Введите первое число: ").upper())
+b = list(input("Введите второе число: ").upper())
+
+print("Сумма:", dec_to_hex(hex_to_dec(a) + hex_to_dec(b)))
+print("Умножение:", dec_to_hex(hex_to_dec(a) * hex_to_dec(b)))
